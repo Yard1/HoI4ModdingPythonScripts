81f06e72e9dc384c4698d182cb128b8b9a1020ad
Yard1 <antoni.baum@gmail.com>
2018-09-26 18:47:58 +0200

    Not yet done

--- python3/hoi4statemapgenerator.py
+++ python3/hoi4statemapgenerator.py
@@ -6,6 +6,7 @@ import re
 from collections import defaultdict
 import random
 import pickle
+import traceback
 
 try:
     from PIL import Image, ImageDraw, ImageFont
@@ -16,6 +17,16 @@ try:
     import numpy as np
 except:
     sys.exit("Requires numpy pip package to be installed. Run:\npip install numpy\nor\npip3.6 install numpy\ndepending on your installation and start the script again.\nMore info on installing packages: https://docs.python.org/3/installing/index.html")
+
+#try:
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+#except:
+#    sys.exit("Requires seaborn pip package to be installed. Run:\npip install seaborn\nor\npip3.6 install seaborn\ndepending on your installation and start the script again.\nMore info on installing packages: https://docs.python.org/3/installing/index.html")
+
+
 
 #############################
 ###
@@ -91,6 +102,23 @@ def generate_new_color(existing_colors,pastel_factor = 0.5):
 
 #############################
 
+providstate = {}
+
+class State():
+    def __init__(self, state_id, provinces, manpower, owner, category, industrial_complex=0, arms_factory=0, infrastructure=0, dockyard=0):
+        self.state_id = state_id
+        self.provinces = provinces
+        self.manpower = manpower
+        self.owner = owner
+        self.category = category
+        self.industrial_complex = industrial_complex
+        self.arms_factory = arms_factory
+        self.infrastructure = infrastructure
+        self.dockyard = dockyard
+        self.pixels = 0
+        for province in provinces:
+            providstate[province] = self
+
 def load_provinces(name):
     print("Reading file " + name + "...")
     im = Image.open(name)
@@ -101,18 +129,79 @@ def load_definition(name):
     with open(name, "r") as f:
         lines = f.read().splitlines()
     provinces = {}
+    provinces_rev = {}
     for line in lines:
         line = line.split(";")
         provinces[int(line[0])] = (int(line[1]), int(line[2]), int(line[3]))
-    return provinces
+        provinces_rev[(int(line[1]), int(line[2]), int(line[3]))] = int(line[0])
+    return (provinces, provinces_rev)
 
 def load_state_file(name, states_dict):
     print("Reading file " + name + "...")
     with open(name, "r") as f:
         file_str = f.read()
-    state_id = int(re.search(r"(?:id\s*=\s*)([0-9]+)", file_str).group(1))
-    province_ids = [int(x) for x in re.search(r"(?:provinces\s*=\s*\{)((\s|.)*?)(?:\})", file_str).group(1).split()]
-    states_dict[state_id] = province_ids
+    if not file_str:
+        return
+    try:
+        state_id = int(re.search(r"id\s*=\s*([0-9]+)", file_str, re.IGNORECASE).group(1))
+        province_ids = [int(x) for x in re.search(r"(?:provinces\s*=\s*\{)((\s|.)*?)(?:\})", file_str, re.IGNORECASE).group(1).split()]
+        manpower = int(re.search(r"(?:manpower\s*=\s*)([0-9]+)", file_str, re.IGNORECASE).group(1))
+        try:
+            owner = re.search(r"(?:owner\s*=\s*)(\"?[a-zA-Z]{3}\"?)", file_str, re.IGNORECASE).group(1)
+        except:
+            owner = re.search(r"(?:controller\s*=\s*)(\"?[a-zA-Z]{3}\"?)", file_str, re.IGNORECASE).group(1)
+        category = re.search(r"(?:category\s*=\s*)(\"?[a-zA-Z_]+\"?)", file_str, re.IGNORECASE).group(1)
+        industrial_complex = re.search(r"(?:industrial_complex\s*=\s*)([0-9]+)", file_str, re.IGNORECASE)
+        if industrial_complex:
+            industrial_complex = int(industrial_complex.group(1))
+        else:
+            industrial_complex = 0
+        arms_factory = re.search(r"(?:arms_factory\s*=\s*)([0-9]+)", file_str, re.IGNORECASE)
+        if arms_factory:
+            arms_factory = int(arms_factory.group(1))
+        else:
+            arms_factory = 0
+        infrastructure = re.search(r"(?:infrastructure\s*=\s*)([0-9]+)", file_str, re.IGNORECASE)
+        if infrastructure:
+            infrastructure = int(infrastructure.group(1))
+        else:
+            infrastructure = 0
+        dockyard = re.search(r"(?:dockyard\s*=\s*)([0-9]+)", file_str, re.IGNORECASE)
+        if dockyard:
+            dockyard = int(dockyard.group(1))
+        else:
+            dockyard = 0
+        states_dict[state_id] = State(state_id, province_ids, manpower, owner, category, industrial_complex, arms_factory, infrastructure, dockyard)
+    except:
+        print("File %s failed to load!" % name)
+        print(file_str)
+        traceback.print_exc()
+
+def load_pdx_colors_file(name):
+    print("Reading file " + name + "...")
+    with open(name, "r") as f:
+        file_str = f.read()
+    pdx_colors = re.findall(r"([A-Z]{3})\s*=\s*{\s*color\s*=\s*rgb\s*{([\s0-9]*)\s*}", file_str, re.IGNORECASE)
+    pdx_colors_hsv = re.findall(r"([A-Z]{3})\s*=\s*{\s*color\s*=\s*hsv\s*{([\s0-9\.]*)\s*}", file_str, re.IGNORECASE)
+    colors = {}
+    for color in pdx_colors:
+        colors[color[0]] = [int(x) for x in color[1].split()]
+    for color in pdx_colors_hsv:
+        colors[color[0]] = [round(255 * float(x)) for x in color[1].split()]
+    return colors
+
+def count_colors(states_dict, provinces_rev, provinces_image):
+    pixels = provinces_image.load()
+    totpix = 0
+    statpix = 0
+    for i in range(provinces_image.size[0]):
+        for j in range(provinces_image.size[1]):
+            totpix += 1
+            provid = provinces_rev[pixels[i, j]]
+            if provid in providstate:
+                statpix += 1
+                providstate[provid].pixels += 1
+    print(totpix, statpix)
 
 def create_states_map(colors_replacement_dict, provinces_image, water_color):
     water_color = (water_color[0], water_color[1], water_color[2])
@@ -177,11 +266,91 @@ def get_colors(name):
         except:
             pass
         print("Saving file " + name + "...")
-        with open(name, "rb") as handle:
-            pickle.dump(colors, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        try:
+            with open(name, "wb") as handle:
+                pickle.dump(colors, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        except:
+            print("Could not save file " + name + "! Continuing...")
 
-    colors.insert(0, [(1/255)*BLUE_RBG[0], (1/255)*BLUE_RBG[1], (1/255)*BLUE_RBG[2]])
     return colors
+
+def get_sequential_colors(space, palette="Reds"):
+    palette = sns.color_palette(palette, space)
+    for idx, item in enumerate(palette):
+        palette[idx] = [int(round(255 * x)) for x in item]
+    return palette
+
+def get_state_color(value, space, colors):
+    for idx in range(0, len(colors)-1):
+        if space[idx] <= value <= space[idx+1]:
+            return colors[idx]
+    return colors[0]
+
+def get_manpower_list(states_dict):
+    manpower_list = []
+    for state_id, state in states_dict.items():
+        manpower_list.append(state.manpower/state.pixels)
+    return sorted(manpower_list)
+
+def get_total_factories_list(states_dict):
+    total_factories_list = []
+    for state_id, state in states_dict.items():
+        total_factories_list.append(state.industrial_complex + state.arms_factory + state.dockyard)
+    return sorted(total_factories_list)
+
+def get_civ_factories_list(states_dict):
+    civ_factories_list = []
+    for state_id, state in states_dict.items():
+        civ_factories_list.append(state.industrial_complex)
+    return sorted(civ_factories_list)
+
+def get_mil_factories_list(states_dict):
+    mil_factories_list = []
+    for state_id, state in states_dict.items():
+        mil_factories_list.append(state.arms_factory)
+    return sorted(mil_factories_list)
+
+def get_infra_list(states_dict):
+    infra_list = []
+    for state_id, state in states_dict.items():
+        infra_list.append(state.infrastructure)
+    return sorted(infra_list)
+
+def get_dockyards_list(states_dict):
+    dockyards_list = []
+    for state_id, state in states_dict.items():
+        dockyards_list.append(state.dockyard)
+    return sorted(dockyards_list)
+
+def generate_legend_and_colors(steps, data_list, title_str, mode, palette="Reds"):
+    if mode == 1:
+        steps = np.linspace(0, 1, num=steps)
+        space = np.quantile(data_list, steps)
+        colors = get_sequential_colors(len(space), palette)
+    else:
+        space = np.linspace(0, data_list[-1], num=steps, dtype=int)
+        print(space)
+        colors = get_sequential_colors(steps, palette)
+    if mode == 1:
+        space[0] = 0
+    labels = []
+    if mode == 1:
+        for idx, label in enumerate(space):
+            if idx < len(space)-1:
+                labels.append("%d - %d" % (int(label), int(space[idx+1])))
+            else:
+                labels.append("%d+" % (int(label)))
+        le = len(space)-1
+    else:
+        labels = space
+        le = len(space)
+    fig = plt.figure()
+    patches = [
+        mpatches.Patch(color=color, label=label)
+        for label, color in zip(labels, sns.color_palette(palette, le))]
+    fig.legend(patches, labels, loc='center', title=title_str, frameon=False)
+    fig.savefig('legend_%s' % args.output, bbox_inches='tight')
+    return (colors, space)
 
 #############################
 
@@ -202,6 +371,15 @@ parser.add_argument( '-nid', '--no_ids', action='store_true', required=False, de
                     help='Do not put IDs on the map (Default: False)')
 
 args = parser.parse_args()
+water_color = [(1/255)*BLUE_RBG[0], (1/255)*BLUE_RBG[1], (1/255)*BLUE_RBG[2]]
+
+#print(sns.color_palette("Blues"))
+
+load_pdx_colors_file("colors.txt")
+
+mode = 6
+manpower_steps = 10
+factories_steps = 10
 
 try:
     dir = readable_dir(args.states)
@@ -214,26 +392,81 @@ for file in os.listdir(states_path):
     if file.endswith(".txt"):
         load_state_file(os.path.join(states_path, file), states_dict)
 
-colors = get_colors(args.colors)
-
 provinces = load_definition(args.definition)
-
+provinces_rev = provinces[1]
+provinces = provinces[0]
 province_map = load_provinces(args.provinces)
+count_colors(states_dict, provinces_rev, province_map)
+
+if mode == 1:
+    manpower_list = get_manpower_list(states_dict)
+    lc = generate_legend_and_colors(manpower_steps, manpower_list, "Population per pixel/7.114km^2", mode)
+    colors = lc[0]
+    space = lc[1]
+elif mode == 2:
+    colors = load_pdx_colors_file(args.colors)
+elif mode == 3:
+    factories_list = get_total_factories_list(states_dict)
+    factories_steps = factories_list[-1]+1
+    lc = generate_legend_and_colors(factories_steps, factories_list, "Total Factories in State", mode)
+    colors = lc[0]
+    space = lc[1]
+elif mode == 4:
+    factories_list = get_civ_factories_list(states_dict)
+    factories_steps = factories_list[-1]+1
+    lc = generate_legend_and_colors(factories_steps, factories_list, "Civilian Factories in State", mode, "Oranges")
+    colors = lc[0]
+    space = lc[1]
+elif mode == 5:
+    factories_list = get_mil_factories_list(states_dict)
+    factories_steps = factories_list[-1]+1
+    lc = generate_legend_and_colors(factories_steps, factories_list, "Military Factories in State", mode, "Greens")
+    colors = lc[0]
+    space = lc[1]
+elif mode == 6:
+    factories_list = get_infra_list(states_dict)
+    factories_steps = factories_list[-1]+1
+    lc = generate_legend_and_colors(factories_steps, factories_list, "Infrastructure in State", mode, "Oranges")
+    colors = lc[0]
+    space = lc[1]
+elif mode == 7:
+    factories_list = get_infra_list(states_dict)
+    factories_steps = factories_list[-1]+1
+    lc = generate_legend_and_colors(factories_steps, factories_list, "Dockyards in State", mode, "Blues")
+    colors = lc[0]
+    space = lc[1]
+else:
+    colors = get_colors(args.colors)
 
 colors_replacement_dict = {}
 
-for state_id, state_provinces in states_dict.items():
-    color = [round(255 * x) for x in colors.pop()]
+for state_id, state in states_dict.items():
+    if mode == 1:
+        color = get_state_color(state.manpower/state.pixels, space, colors)
+    elif mode == 2:
+        color = colors[state.owner]
+    elif mode == 3:
+        color = get_state_color(state.industrial_complex+state.arms_factory+state.dockyard, space, colors)
+    elif mode == 4:
+        color = get_state_color(state.industrial_complex, space, colors)
+    elif mode == 5:
+        color = get_state_color(state.arms_factory, space, colors)
+    elif mode == 6:
+        color = get_state_color(state.infrastructure, space, colors)
+    elif mode == 7:
+        color = get_state_color(state.dockyard, space, colors)
+    else:
+        color = [round(255 * x) for x in colors.pop()]
 
     #print("STATE %s: COLOR: %s" % (str(state_id), color))
-    for province in state_provinces:
+    for province in state.provinces:
         colors_replacement_dict[provinces[province]] = ((color[0], color[1], color[2]), state_id)
 
 print("Generating map image - this may take a while...")
 if args.no_ids:
-    create_states_map(colors_replacement_dict, province_map, [round(255 * x) for x in colors[0]])
+    create_states_map(colors_replacement_dict, province_map, [round(255 * x) for x in water_color])
 else:
-    create_states_map_with_id(colors_replacement_dict, province_map, [round(255 * x) for x in colors[0]], args.font)
+    create_states_map_with_id(colors_replacement_dict, province_map, [round(255 * x) for x in water_color], args.font)
 
 province_map.show()
 print("Saving file " + args.output + "...")
